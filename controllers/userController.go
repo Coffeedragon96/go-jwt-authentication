e215f0e77b6cdc2d21eef4fb3d6384808636985a
Shrey Raval <ravalshrey@gmail.com>
2022-01-31 15:36:22 +0530

    Part 3

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,8 +1,17 @@
 package controllers
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/coffeedragon96/go-jwt-authentication/database"
+	helper "github.com/coffeedragon96/go-jwt-authentication/helpers"
+	"github.com/coffeedragon96/go-jwt-authentication/models"
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -17,7 +26,45 @@ func VerifyPassword() {
 
 }
 
-func SignUp() {
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while checking for the email"})
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "This email already exists"})
+		}
+
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while checking for the phone number"})
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "This phone number already exists"})
+		}
+
+	}
 
 }
 
@@ -29,6 +76,24 @@ func GetUsers() {
 
 }
 
-func GetUser() {
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userID := c.Param("user_id")
 
+		if err := helper.MatchUserTypeToUid(c, userID); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userID}).Decode(&user)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		c.JSON(http.StatusOK, user)
+	}
 }
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,12 +2,11 @@ package routes
 
 import (
 	"github.com/coffeedragon96/go-jwt-authentication/controllers"
-	controller "github.com/coffeedragon96/go-jwt-authentication/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.authenticate())
-	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users", controllers.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
 }
